821370f865b4acb4bc0e0ee12e4a3b90d5df6c71
Th1b0lt <thibmure@orange.fr>
2024-05-02 09:29:50 +0200

    Tableaux d'indice max

--- attribut.c
+++ attribut.c
@@ -12,6 +12,7 @@
     attribut a;
     char *** pointeur;
     int ** compteur;
+    int * nb_elements_par_colonne;
     //Initialisation de nos tableaux)
     compteur=malloc(sizeof(int *)*(s.nbr_exemples+1));
     if (compteur == NULL){
@@ -20,6 +21,11 @@
     }
     pointeur=malloc(sizeof(char***)*(s.nbr_exemples+1));
     if (pointeur == NULL){
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(1);
+    }
+    nb_elements_par_colonne = malloc(sizeof(int) * (s.nbr_exemples + 1));
+    if (nb_elements_par_colonne == NULL) {
         fprintf(stderr, "Memory allocation failed.\n");
         exit(1);
     }
@@ -34,44 +40,50 @@
             fprintf(stderr, "Memory allocation failed.\n");
             exit(1);
         }
+        nb_elements_par_colonne[i] = 0;
         for (int j = 0; j < s.nbr_exemples + 1; j++) {
             pointeur[i][j] = NULL;
         }
     }
-    a.index=0;
     a.pointeur=pointeur;
     a.compteur=compteur;
+    a.nb_elements_par_colonne=nb_elements_par_colonne;
+    a.nb_colonnes=0;
     return a;
 }
 
 
 //Utilisé pour le test uniquement
 void affiche_attribut(attribut attr){
-    int i=0;
-    while(attr.pointeur[i][0]!=NULL){ 
-        
-        int j=0;
-        while(attr.pointeur[i][j]!=NULL){
+    for (int i=0;i<attr.nb_colonnes;i++){
+        for(int j=0;j<=attr.nb_elements_par_colonne[i];j++){
             if (j==0){
                 printf("Attribut: %s nombre : %d\n",attr.pointeur[i][j],attr.compteur[i][j]);
             }
             else{
                 printf("valeur : %s nombre : %d\n",attr.pointeur[i][j],attr.compteur[i][j]);
             }
-            j++;
         }
-        i++;
     }
 }
+void renitialise_attribut(attribut attr){
+    for (int i=0;i<attr.nb_colonnes;i++){
+        attr.nb_elements_par_colonne[i]=1;
+    }
+    attr.nb_colonnes=0;
+}
+
+
 
 attribut rempli_attribut(stockage s,int index_attribut, int debut, int fin){
     attribut attr=initialise_attribut(s);
+    renitialise_attribut(attr);
     for (int e=debut;e<fin;e++){ //PArcours tous les exemples
         int i=0;
         int exit1=0; //exit de la première boucle
         int exit2=0; //exit de la deuxième boucle
         int j=1;
-        while(exit1==0 && attr.pointeur[i][0]!=NULL){
+        while(exit1==0 && i<attr.nb_colonnes){
             if (strcmp(s.tableau[e][index_attribut],attr.pointeur[i][0])==0){
                 attr.compteur[i][0]++;
                 exit1=1;
@@ -85,6 +97,7 @@ attribut rempli_attribut(stockage s,int index_attribut, int debut, int fin){
         if (exit1==0){
             attr.pointeur[i][0]=s.tableau[e][index_attribut];
             attr.compteur[i][0]=1;
+            attr.nb_colonnes++;
             printf("Sa passe ici 1 : %s\n",attr.pointeur[i][0]);
         }
         
@@ -102,13 +115,15 @@ attribut rempli_attribut(stockage s,int index_attribut, int debut, int fin){
         if (exit2==0){
             attr.pointeur[i][j]=s.tableau[e][s.nbr_attributs-1];
             attr.compteur[i][j]=1;
+            attr.nb_elements_par_colonne[i]++;
             printf("Sa passe ici 4 %s \n ",attr.pointeur[i][j]);
         }
         
         
 
     }
-
+    
+    affiche_attribut(attr);
     return attr;
 }
 
--- attribut.h
+++ attribut.h
@@ -1,7 +1,8 @@
 #ifndef attribut_H
 #define attribut_H
 typedef struct{
-    int index;
+    int nb_colonnes;
+    int *nb_elements_par_colonne;
     int ** compteur;
     char *** pointeur;
 } attribut;
