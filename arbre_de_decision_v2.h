d3652023ed215358e81963b6dac0d078ccebc5b6
ValentinS65 <serangevalentin@gmail.com>
2024-05-07 14:26:56 +0200

    trie

--- arbre_de_decision_v2.h
+++ arbre_de_decision_v2.h
@@ -8,6 +8,7 @@ typedef struct stockage{
     int nbr_exemples;
     int nbr_etiquette;
     int nbr_valeur_max_attribut;
+    int * ordre_exemple;
 }stockage;
 int inList(char ** l, char * mot,int taille);
 char ** cherche_etiquette(stockage *s);
@@ -15,9 +16,9 @@ int notInList(int i,int j,stockage s,int * dejaVu, int taille);
 int cherche_valeur_max(stockage s);
 stockage extraction_fichier(char * fichier);
 void afficher_tableau(stockage s);
-
 void afficher_etiquette(stockage s);
 void free_stockage(stockage s);
 int ** init_tab(int lignes,int colonnes);
+void afficher_trie(stockage s);
 void Trie_Stockage_attribut(stockage *s, int attributchoisie,int debut, int fin);
 #endif 
